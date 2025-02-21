971f1c9217b328b42493516214dc2d4466b9cdd9
dzjyyds666 <2529619036@qq.com>
2025-02-22 01:06:55 +0800

    init commit

---
+++ user/internal/logic/handlerloginlogic.go
@@ -0,0 +1,46 @@
+package logic
+
+import (
+	"context"
+	"github/dzjyyds666/online_study_server/user/internal/svc"
+	"github/dzjyyds666/online_study_server/user/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type HandlerLoginLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewHandlerLoginLogic(ctx context.Context, svcCtx *svc.ServiceContext) *HandlerLoginLogic {
+	return &HandlerLoginLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *HandlerLoginLogic) HandlerLogin(req *types.LoginReq) (resp *types.HttpResponse, err error) {
+	// todo: add your logic here and delete this line
+	name := "dzj"
+	password := "123456"
+	if req.Username == name && req.Password == password {
+		resp = &types.HttpResponse{
+			Code: 200,
+			Data: types.LoginResp{
+				Token:  "123456",
+				UserId: "123456",
+			},
+			Msg: "success",
+		}
+	} else {
+		resp = &types.HttpResponse{
+			Code: 400,
+			Data: "fail",
+			Msg:  "fail",
+		}
+	}
+	return
+}
---
+++ user/internal/svc/servicecontext.go
@@ -0,0 +1,15 @@
+package svc
+
+import (
+	"github/dzjyyds666/online_study_server/user/internal/config"
+)
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
